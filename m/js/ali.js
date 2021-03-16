508dc4dc4a26437dc52df3465884fa35911279e1
mobaiwar <73160262+mobaiwar@users.noreply.github.com>
2021-03-16 17:21:18 +0800

    Add files via upload

---
+++ m/js/ali.js
@@ -0,0 +1,74 @@
+﻿var u = navigator.userAgent;
+var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+var isbaidu = u.indexOf('baidu') > -1 || u.indexOf('Baidu') > -1; 
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+if(isAndroid && !isbaidu){
+document.writeln("<style>body{font-family:\'Microsoft YaHei\',SimSun,\'\/\/5b8b\/\/4f53\',sans-serif;}body,html,ul,li,p,h1,h2,h3,h4,h5,p,button,form,input{margin:0;padding:0;}ul,li{list-style: none;}a{text-decoration:none;}img{vertical-align: middle;}html,body{min-width:320px;width:100%;}input:focus, button:focus, select:focus, textarea:focus {outline: 0;}.clearfix{*zoom:1;}.clearfix:after{display: block;content:\'\';clear:both;}.ad-18{padding:0px 8px 6px;border:1px solid #ddd}.ad-18 div{float:left;width:49%;position:relative;overflow: hidden;margin-top:6px}.ad-18 div:nth-child(2n-1){margin-right:2%;}.ad-18 img{width: 100%;height:100%}.ad-18 span{position:absolute;left:0;bottom:0;display: block;width:100%;height:24px;text-indent: 28px;color:#fff;line-height: 24px;font-size:0.8rem;background-color: rgba(0,0,0,.5)}.ad-18 i{position:absolute;left:4px;top:3px;display: block;width:18px;height:18px;background: url(https://img.alicdn.com/imgextra/i2/1052185841/TB2PbjxkilnpuFjSZFgXXbi7FXa_!!1052185841.png) no-repeat;background-size: 18px 18px;}.ad-18 em{position:absolute;right:5px;top:0;color:#d5d5d5;line-height: 24px;font-size:0.6rem;font-style: normal;}.search{position:relative;width: 100%;height:32px;background-color: #f5f5f5;}.search i{display: block;width: 4px;height:18px;position:absolute;top:7px;left:7px;background-color: #ff9191;}.s{margin-left:20px;color:#333;height:32px;border:none;box-sizing: border-box;font-weight:bold;font-size:16px;line-height: 32px;}.search a{font-size:12px;line-height: 32px;color:#bbb;position:absolute;right:12px;top:0;}.fixdxx{width: 100%;height:100%;position: fixed;top: 0;left: 0;z-index:210000000;}</style>");
+var data = new Array();
+var array = new Array();
+var data = [
+
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2bk0oaY_0UKFjy1XaXXbKfXXa_!!1052185841.gif", "动态图国产自怕"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2o0Wlkb4npuFjSZFmXXXl4FXa_!!1052185841.gif", "AV女忧gif图"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB2ZGttkkqvpuFjSZFhXXaOgXXa_!!1052185841.gif", "动态图亚洲激情"],
+    ["https://img.alicdn.com/imgextra/i2/1052185841/TB2Fov4iCtkpuFjy0FhXXXQzFXa_!!1052185841.gif", "极品女忧邪恶图"],
+    ["https://img.alicdn.com/imgextra/i2/1052185841/TB2jj0tkdBopuFjSZPcXXc9EpXa_!!1052185841.gif", "动态图高清无码"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB2SvwciB4lpuFjy1zjXXcAKpXa_!!1052185841.gif", "成人剧场邪恶图"],
+    ["https://img.alicdn.com/imgextra/i4/1052185841/TB2yMwJiCFjpuFjSszhXXaBuVXa_!!1052185841.gif", "日本女忧动态图"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB2rG1ekbxmpuFjSZJiXXXauVXa_!!1052185841.gif", "邪恶图免费观看"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB2EswHd4RDOuFjSZFzXXcIipXa_!!1052185841.gif", "动态图在线播放"],
+    ["https://img.alicdn.com/imgextra/i4/1052185841/TB2TbxVkohnpuFjSZFpXXcpuXXa_!!1052185841.gif", "日韩爽片邪恶图"],
+    ["https://img.alicdn.com/imgextra/i2/1052185841/TB2E.lSkolnpuFjSZFjXXXTaVXa_!!1052185841.gif", "动态图亚洲在线"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2FX.viq8lpuFjy0FpXXaGrpXa_!!1052185841.gif", "亚洲无码邪恶图"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2b7EniB0kpuFjSsppXXcGTXXa_!!1052185841.gif", "动态图高清无码"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB26G81kdFopuFjSZFHXXbSlXXa_!!1052185841.gif", "在线播放gif图"],
+    ["https://img.alicdn.com/imgextra/i4/1052185841/TB2ISrLiCXlpuFjy0FeXXcJbFXa_!!1052185841.gif", "动态图免费观看"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB2lBT_iCxjpuFjSszeXXaeMVXa_!!1052185841.gif", "成人剧场邪恶图"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2yK8wkhtmpuFjSZFqXXbHFpXa_!!1052185841.gif", "动态图在线播放"],
+    ["https://img.alicdn.com/imgextra/i3/1052185841/TB2Ck_2iB0lpuFjSszdXXcdxFXa_!!1052185841.gif", "成人剧场邪恶图"],
+    ["https://img.alicdn.com/imgextra/i4/1052185841/TB2s7pFkbJmpuFjSZFwXXaE4VXa_!!1052185841.gif", "动态图AV鏄庢槦"],
+    ["https://img.alicdn.com/imgextra/i1/1052185841/TB290dIkmVmpuFjSZFFXXcZApXa_!!1052185841.gif", "成人剧场邪恶图"],
+    ["https://img.alicdn.com/imgextra/i4/1052185841/TB2v_4zkbBnpuFjSZFGXXX51pXa_!!1052185841.gif", "动态图免费观看"]];
+
+
+function getArray(count, maxs, mins) {
+    while (array.length < count) {
+        var temp = getRandom(maxs, mins);
+        if (!serch(array, temp)) {
+            array.push(temp);
+        }
+    }
+    return array;
+}
+
+function getRandom(maxs, mins) {
+    return Math.round(Math.random() * (maxs - mins)) + mins;
+}
+
+function serch(array, num) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+document.writeln("<div class=\'ad-18 clearfix\'>");
+var d = getArray(2, 20, 0);
+var e = navigator.userAgent.toLowerCase(),
+    f = e.match(/ipad/i) == "ipad",
+    b = e.match(/iphone/i) == "iphone",
+    c = e.match(/android/i) == "android";
+if (f || b) {
+    var url = 'https://playd.zgwlzho.cn/zLink/?t=zb_17';
+}else{
+    var url = 'https://playd.zgwlzho.cn/zLink/?t=zb_17';
+}
+for (x in d) {
+    var img = data[d[x]][0];
+    var txt = data[d[x]][1];
+    document.writeln("<div><a target='_blank' href='" + url + "'><img src=" + img + "><span><i></i>" + txt + "<em>119:04</em></span></a></div>");
+}
+
+document.writeln("</div>");
+}
